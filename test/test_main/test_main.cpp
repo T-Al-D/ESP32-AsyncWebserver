d6f6ecee3dc591985e032ff22870a249690b64e0
T-Al-D <t.private@gmx.net>
2025-03-26 16:41:21 +0100

    first testing examples (currently not useful)

---
+++ test/test_main/test_main.cpp
@@ -0,0 +1,48 @@
+#include <unity.h>
+
+void setUp(void)
+{
+    // set stuff up here
+}
+
+void tearDown(void)
+{
+    // clean stuff up here
+}
+/////
+
+void test_example(void)
+{
+    // Test assertion
+    TEST_ASSERT_EQUAL(1, 1);
+}
+
+void test_example2(void)
+{
+    TEST_ASSERT_NOT_EQUAL(1, 0);
+}
+
+////
+void setup()
+{
+    UNITY_BEGIN();
+}
+
+void loop()
+{
+    RUN_TEST(test_example2);
+    RUN_TEST(test_example);
+    UNITY_END();
+}
+
+int main(int argc, char** argv)
+{
+    // UNITY_BEGIN();
+
+    // Run your test functions here
+    //RUN_TEST(test_example);
+    
+
+    // UNITY_END();
+    return 0;
+}
