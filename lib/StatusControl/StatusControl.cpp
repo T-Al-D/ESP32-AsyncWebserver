e4171af9dedc32801b761ab310511bc26ab6946d
T-Al-D <t.private@gmx.net>
2025-04-08 08:58:03 +0200

    first variables for input

--- lib/StatusControl/StatusControl.cpp
+++ lib/StatusControl/StatusControl.cpp
@@ -20,7 +20,27 @@ const short LED2 = 45;
 bool button1Status = false;
 bool button2Status = false;
 
-// "imoport" global variables
+/////////////////////////// FACTORY SIMULATION //////////////////////////
+// 5 inputs for the factory simulation
+const short INPUT_1 = 7;
+const short INPUT_2 = 6;
+const short INPUT_3 = 5;
+const short INPUT_4 = 4;
+const short INPUT_5 = 3;
+
+short conveyorBelt1Status = false;
+short conveyorBelt2Status = false;
+short conveyorBelt3Status = false;
+short conveyorBelt4Status = false;
+
+short slideMotor1Status = false;
+short slideMotor2Status = false;
+
+short taskMotor1Status = false;
+short taskMotor2Status = false;
+/////////////////////////// FACTORY SIMULATION //////////////////////////
+
+// "import" global variables
 extern String output;
 
 void setPins()
@@ -28,6 +48,13 @@ void setPins()
     // set inputs
     pinMode(PHOTO_RESISTOR_1, INPUT);
     pinMode(PHOTO_RESISTOR_2, INPUT);
+
+    // default:0 , if connect to +: 1
+    pinMode(INPUT_1, INPUT_PULLDOWN);
+    pinMode(INPUT_2, INPUT_PULLDOWN);
+    pinMode(INPUT_3, INPUT_PULLDOWN);
+    pinMode(INPUT_4, INPUT_PULLDOWN);
+    pinMode(INPUT_5, INPUT_PULLDOWN);
 
     // set outputs
     pinMode(LED1, OUTPUT);
@@ -54,6 +81,35 @@ void readSensors()
         button2Status = true;
         output = "B2 stat\nchange:\nON";
         Serial.println("Photo_Sensor2 passed!");
+    }
+
+    // read the inputs for factory control
+    bool input1Digtital = digitalRead(INPUT_1);
+    bool input2Digtital = digitalRead(INPUT_2);
+    bool input3Digtital = digitalRead(INPUT_3);
+    bool input4Digtital = digitalRead(INPUT_4);
+    bool input5Digtital = digitalRead(INPUT_5);
+
+    // react to inputs
+    if (input1Digtital) {
+        Serial.println("INPUT_1 digital: " + String(input1Digtital));
+        Serial.println("--------------");
+    }
+    if (input2Digtital) {
+        Serial.println("INPUT_2 digital: " + String(input2Digtital));
+        Serial.println("--------------");
+    }
+    if (input3Digtital) {
+        Serial.println("INPUT_3 digital: " + String(input3Digtital));
+        Serial.println("--------------");
+    }
+    if (input4Digtital) {
+        Serial.println("INPUT_4 digital: " + String(input4Digtital));
+        Serial.println("--------------");
+    }
+    if (input5Digtital) {
+        Serial.println("INPUT_5 digital: " + String(input5Digtital));
+        Serial.println("--------------");
     }
 
     /* for debug
--- lib/StatusControl/StatusControl.h
+++ lib/StatusControl/StatusControl.h
@@ -16,6 +16,28 @@ extern const short PHOTO_RESISTOR_2;
 extern bool button1Status;
 extern bool button2Status;
 
+/////////////////////////// FACTORY SIMULATION //////////////////////////
+// 5 inputs for the factory simulation
+extern const short INPUT_1;
+extern const short INPUT_2;
+extern const short INPUT_3;
+extern const short INPUT_4;
+extern const short INPUT_5;
+
+// 4 conveyor belts, 2 Slidemotors, 2 taskmotors need to be moved (action)
+// in THIS ORDER !
+extern short conveyorBelt1Status;
+extern short conveyorBelt2Status;
+extern short conveyorBelt3Status;
+extern short conveyorBelt4Status;
+
+extern short slideMotor1Status;
+extern short slideMotor2Status;
+
+extern short taskMotor1Status;
+extern short taskMotor2Status;
+/////////////////////////// FACTORY SIMULATION //////////////////////////
+
 void setPins();
 void readSensors();
 void writeOutputs();
